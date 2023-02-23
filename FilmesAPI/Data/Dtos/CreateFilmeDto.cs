ae1cf0b9a982816e773e6ac7f7999165e3c4faa8
Patrick Ferrari <patrick_o_ferrari@hotmail.com>
2023-02-23 18:03:21 -0300

    Adicionando projeto ao repositório

---
+++ FilmesAPI/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class CreateFilmeDto
+    {
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
+        public string? Titulo { get; set; }
+        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+        public string? Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 360")]
+        public int Duracao { get; set; }
+    }
+}
